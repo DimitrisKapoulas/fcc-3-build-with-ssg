62d1936087d22140d05ab0e7e383e34190e3bbba
mitsos <kapoulasd@gmail.com>
2020-03-18 20:18:52 +0200

    CDN localized

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,13 +1,25 @@
 const axios = require("axios");
+const countries = require("./countries.json");
 require('dotenv').config();
 
 
-module.exports =  async function() {
+async function getNews(country) {
   try {
-    const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=gr&apiKey=${process.env.API_KEY}&pageSize=6`);
-    return response.data;
+    const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.API_KEY}&pageSize=6`);
+    return {
+      "country": country,
+      "articles": response.data.articles
+    }
   } catch (error) {
     console.error(error);
   }
+}
 
+module.exports =  async function() {
+
+  var newsPromises = countries.map(getNews);
+  return Promise.all(newsPromises).then( newsObjects => {
+    console.log('newsObjects:', newsObjects);
+    return [].concat.apply([], newsObjects);
+  });
 };
